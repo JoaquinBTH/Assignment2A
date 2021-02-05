3d5836a23b23bdbc2ba79b81d3bb39531b727eac
Joaquin <joaquinzache@hotmail.com>
2021-02-05 12:40:28 +0100

    Recieves first message, sends back OK as response and recieves operation and numbers to use.

--- clientmain.cpp
+++ clientmain.cpp
@@ -6,6 +6,8 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <string.h>
+#include <string>
 // Enable if you want debugging to be printed, see examble below.
 // Alternative, pass 
 #define DEBUG
@@ -52,12 +54,42 @@ int main(int argc, char *argv[]){
     return 2;
   }
 
+  //Create buffer
   char buf[256];
-
   memset(buf, 0, 256);
-  int recieve = recv(sock, buf, sizeof buf, 0);
-  
-  
 
+  //Recieve a message from the server
+  int recieve = recv(sock, buf, sizeof buf, 0);
+  if(recieve == -1)
+  {
+    printf("Recieve Failed!");
+    return 3;
+  }
+  else if(strcmp(buf, "TEXT TCP 1.0\n") == 0)
+  {
+    printf("Wrong Client Application!");
+    return 4;
+  }
+
+  //Send "OK" as a response to the server
+  std::string ok = "OK\n";
+  int response = send(sock, ok.c_str(), ok.size() + 1, 0);
+  if(response == -1)
+  {
+    printf("Response Failed!");
+    return 5;
+  }
+
+  //Recieve the operation and numbers
+  memset(buf, 0, 256);
+  recieve = recv(sock, buf, sizeof buf, 0);
+  if(recieve == -1)
+  {
+    printf("Recieve Failed!");
+    return 6;
+  }
+
+  //Close socket
   close(sock);
+  return 0;
 }
