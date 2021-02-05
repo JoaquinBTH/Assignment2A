c68e57495b3ef5ca91ce949b907ceae38f0cbc58
Joaquin <joaquinzache@hotmail.com>
2021-02-05 11:14:47 +0100

    Start to solution

--- clientmain.cpp
+++ clientmain.cpp
@@ -2,7 +2,10 @@
 #include <string.h>
 #include <stdlib.h>
 /* You will to add includes here */
-
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 // Enable if you want debugging to be printed, see examble below.
 // Alternative, pass 
 #define DEBUG
@@ -29,5 +32,32 @@ int main(int argc, char *argv[]){
   printf("Host %s, and port %d.\n",Desthost,port);
 #endif
 
+  sockaddr_in hint;
+  memset(&hint, 0, sizeof hint);
+  hint.sin_family = AF_INET;
+  hint.sin_port = htons(port);
+  inet_pton(AF_INET, Desthost, &hint.sin_addr);
+
+  int sock = socket(AF_INET, SOCK_STREAM, 0);
+  if (sock == -1)
+  {
+    printf("Socket Failed!");
+    return 1;
+  }
+
+  int con = connect(sock, (sockaddr*)&hint, sizeof (hint));
+  if (con == -1)
+  {
+    printf("Connection Failed!");
+    return 2;
+  }
+
+  char buf[256];
+
+  memset(buf, 0, 256);
+  int recieve = recv(sock, buf, sizeof buf, 0);
   
+  
+
+  close(sock);
 }
